8d56c4b6a293c345368aeae577b3e0d5cb17475b
Alessandro Freitas <alessandro_freitas@icloud.com>
2025-03-31 10:52:24 -0300

    fix cors

--- src/app.js
+++ src/app.js
@@ -2,31 +2,30 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const allowedOrigins = 'https://nuxt-hzcz4a9ji-alessandrosfreitas-projects.vercel.app';
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true);
-
-//     if (allowedOrigins.includes(origin)) {
-//       return callback(null, true);
-//     } else {
-//       return callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-// }));
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// });
+const allowedOrigins = [
+  'https://nuxt-hzcz4a9ji-alessandrosfreitas-projects.vercel.app',
+  'http://localhost:3001',
+];
 
 app.use(cors({
-  origin: '*'
+  origin: (origin, callback) => {
+    console.log('Origin recebida:', origin);
+
+    if (!origin) {
+      console.log('Requisição sem origem permitida.');
+      return callback(null, true);
+    }
+
+    if (allowedOrigins.includes(origin)) {
+      console.log(`Origem permitida: ${origin}`);
+      return callback(null, true);
+    } else {
+      console.log(`Origem bloqueada: ${origin}`);
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json());
