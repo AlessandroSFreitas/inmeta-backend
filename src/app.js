e6a36a5ec1ca20a1f1610eea05f340ad880e93fe
Alessandro Freitas <alessandro_freitas@icloud.com>
2025-03-31 10:34:07 -0300

    fix cors

--- src/app.js
+++ src/app.js
@@ -18,6 +18,13 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://nuxt-hzcz4a9ji-alessandrosfreitas-projects.vercel.ap');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 
 // Routes
